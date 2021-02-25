a8c336786fc2d4c2223e0bb16cdfab83eb31ba1a
Shreyansh jain <78850912+shreyansh1406@users.noreply.github.com>
2021-02-25 16:59:58 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,34 @@
+public class Employee
+{
+private String name;
+private String address;
+private String mobile;
+
+public void setname(String name)
+{
+this.name=name;
+}
+public String getname()
+{
+return name;
+}
+
+public void setadd(String address)
+{
+this.address=address;
+}
+public String getadd()
+{
+return address;
+}
+
+public void setmob(String mobile)
+{
+this.mobile=mobile;
+}
+public String getmob()
+{
+return mobile;
+}
+
+}
---
+++ InningsMain.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class InningsMain
+{
+public static void main(String []args)
+{
+Innings i=new Innings();
+i.displayInningsDetails();
+}
+}
