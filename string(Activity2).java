15ff693e01c63ac42687ad5c1ba39f1912ec1a4e
Shreyansh jain <78850912+shreyansh1406@users.noreply.github.com>
2021-02-25 12:23:40 +0530

    Activity2- check last name

---
+++ string(Activity2).java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	String s1, s2;
+    Scanner sc = new Scanner (System.in);
+    System.out.println ("enter first string");
+    s1 = sc.nextLine ();
+    System.out.println ("enter second string");
+    s2 = sc.nextLine ();
+    String a = s1.substring (s1.indexOf(' ')+1);
+    String b = s2.substring (s2.indexOf(' ')+1);
+    if(a.equals(b))
+    System.out.println ("Yes");
+    else 
+    System.out.println ("No");
+    
+}
+}
